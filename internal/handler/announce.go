7e85e9a60377825fcf91c52af3d1adf676aae3aa
Alex Wellnitz <moin@wellnitz-alex.de>
2024-11-28 23:22:40 +0100

    feat: add port validation in parseAnnounceRequest function

--- internal/handler/announce.go
+++ internal/handler/announce.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"encoding/base64"
+	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/alexohneander/GoZilla/internal/database"
 	"github.com/alexohneander/GoZilla/pkg/model"
@@ -32,6 +34,11 @@ func parseAnnounceRequest(c *gin.Context) (model.Peer, error) {
 
 	peer.InfoHash = c.Query("info_hash")
 	peer.PeerID = c.Query("peer_id")
+
+	if peer.InfoHash == "" || peer.PeerID == "" {
+		err := errors.New("invalid request")
+		return model.Peer{}, err
+	}
 	peer.ID = base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s-%s", peer.InfoHash, peer.PeerID)))
 
 	// Get IP Address
@@ -39,6 +46,42 @@ func parseAnnounceRequest(c *gin.Context) (model.Peer, error) {
 		peer.IP = c.Query("ip")
 	} else {
 		peer.IP = c.RemoteIP()
+	}
+
+	if c.Query("port") == "" {
+		err := errors.New("invalid request")
+		return model.Peer{}, err
+	}
+
+	// parse into int32
+	parsedPort, err := strconv.ParseInt(c.Query("port"), 0, 32)
+	if err != nil {
+		return model.Peer{}, err
+	}
+	peer.Port = int32(parsedPort)
+
+	if c.Query("uploaded") != "" {
+		parsedUploaded, err := strconv.ParseInt(c.Query("uploaded"), 0, 64)
+		if err != nil {
+			return model.Peer{}, err
+		}
+		peer.Uploaded = int64(parsedUploaded)
+	}
+
+	if c.Query("downloaded") != "" {
+		parsedDownloaded, err := strconv.ParseInt(c.Query("downloaded"), 0, 64)
+		if err != nil {
+			return model.Peer{}, err
+		}
+		peer.Downloaded = int64(parsedDownloaded)
+	}
+
+	if c.Query("left") != "" {
+		parsedLeft, err := strconv.ParseInt(c.Query("left"), 0, 64)
+		if err != nil {
+			return model.Peer{}, err
+		}
+		peer.Left = int64(parsedLeft)
 	}
 
 	return peer, nil
